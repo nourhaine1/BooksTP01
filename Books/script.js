f28f769663a46bab305ccb51e589cc6f20de5f37
nourhaine1 <ayadinourhaine@gmail.com>
2023-10-01 15:10:41 +0200

    debbuging errors +delete function +make the same modal to add and edit

--- Books/script.js
+++ Books/script.js
@@ -1,6 +1,34 @@
 
 var ListOfBooks = JSON.parse(localStorage.getItem('books'));
+let modal = document.getElementById("exampleModal");
+let modalTitle = document.getElementById("modalTitle");
+let index= null;
+function openModal(type,indexnow) {
+   modal.style.display ="block";
+   var name = document.getElementById('name').value='';
+   var cat = document.getElementById('cat').value='';
+   var auth = document.getElementById('auth').value='';
+   var price = document.getElementById('price').value='';
+   if (type == "add"){
+      modalTitle.textContent="Add book";
+   }
+   else if (type == "edit"){
+      modalTitle.textContent="Edit book";
+      index = indexnow; 
+ let book =ListOfBooks[indexnow];
+ document.getElementById('name').value=book.name;
+ document.getElementById('cat').value=book.cat;
+ document.getElementById('auth').value=book.auth;
+  document.getElementById('price').value=book.price;
+
+   }
+
+}
+function closeModal() {
+   modal.style.display="none";
+}
 function saveToLocalStorage() {
+
    console.log("test of function entry ");
 
    var name = document.getElementById('name').value;
@@ -9,11 +37,19 @@ function saveToLocalStorage() {
    var price = document.getElementById('price').value;
    var newBook = { name: name, cat: cat, auth: auth, price: price };
    console.log(newBook);
-
    var books = JSON.parse(localStorage.getItem('books'));
-   books.push(newBook);
+
+   if (index ===null){
+      books.push(newBook);
+      alert("Book added successfully");
+
+   }
+   else {
+      books[index] = newBook;
+      alert("Book edited successfully");
+
+   }
    localStorage.setItem('books', JSON.stringify(books));
-   alert("Book added successfully");
    window.location.reload();
 }
 
@@ -27,7 +63,9 @@ var html='';
       html += "<td>" + element.name + "</td>"; html += "<td>" + element.auth + 
       "</td>"; html += "<td>" + element.cat + "</td>"; 
       html += "<td>" + element.price+ "</td>";
-      html += "<td>" + '<button class="btn btn-info" onclick="editBook()" > Edit</button>'+'<button class="btn btn-danger" onclick="deleteBook('+index +')"> Delete</button>'+ "</td>";
+      html += "<td>" + ''+'<button type="button" class="btn btn-danger" onclick="deleteBook(' + index + ')"> Delete</button>'+ '<button type="button" class="btn btn-info"  onclick="openModal(\'edit\', ' + index + ')"data-toggle="modal" data-target="#editModal"> Edit</button>'+"</td>";
+     
+     // html += "<td>" + ''+'<button class="btn btn-danger" onclick="deleteBook('+index +')"> Delete</button>'+ "</td>";
       html += "</tr>";
 
        });
@@ -35,16 +73,21 @@ var html='';
        
 }
 
+
 // Call the function to display books from localStorage when the page is loaded
 window.onload = function() {
    getBooksFromLocalStorage();
 };
 
-function deleteBook(index){
- 
+function deleteBook(ind){
+ console.log(" ********************* the index here is " + ind.type);
+index=parseInt(ind);
    ListOfBooks.splice(index, 1); // Remove the book at the given index
   localStorage.setItem("books", JSON.stringify(ListOfBooks));
   alert("Book deleted successfully.");
+  window.location.reload();
+
    //getBooksFromLocalStorage();
   }
- 
+
+//function editBook(index, book) 
